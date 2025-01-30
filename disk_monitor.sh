ce357a315238e53e5eaedcdd97ea3f44ced15345
Abhijeet Kumar <abhijeet7411@gmail.com>
2025-01-31 01:14:41 +0530

    V1 for disk monitoring

---
+++ disk_monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+THRESHOLD=80
+df -h | awk 'NR>1 {print $5 " " $6}' | while read output; do
+  usage=$(echo $output | awk '{print $1}' | sed 's/%//')
+  partition=$(echo $output | awk '{print $2}')
+  if [ "$usage" -ge "$THRESHOLD" ]; then
+    echo "WARNING: Disk space on $partition is at ${usage}%"
+  fi
+done
+
