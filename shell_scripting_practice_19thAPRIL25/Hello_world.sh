1dac5d42dab071f244aa978a6742b5946161975a
Abhijeet Kumar <abhijeet7411@gmail.com>
2025-04-20 12:53:59 +0530

    V2 adding comments

--- shell_scripting_practice_19thAPRIL25/Hello_world.sh
+++ shell_scripting_practice_19thAPRIL25/Hello_world.sh
@@ -3,3 +3,33 @@
 echo "           "
 echo "Hello world"
 
+
+# this is single line comment 
+
+
+<<comment
+
+This is multiline comment 
+
+hello 
+hey 
+hii 
+Yellow 
+
+comment
+
+
+
+<<Hello 
+
+This is
+Multi
+line 
+comment 
+
+Hello
+
+
+echo "got it!"
+
+
