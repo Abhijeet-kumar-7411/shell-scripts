0ae716000e1a286bd0a0adbe00ebc53af0a40d1f
Abhijeet Kumar <abhijeet7411@gmail.com>
2025-04-20 13:13:29 +0530

    Use of Variables

---
+++ shell_scripting_practice_19thAPRIL25/Variable_use.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# we will be using variables in this script
+
+# note that all variable name is in Cap letter
+
+NAME="Abhijeet"
+AGE="25"
+HOSTNAME=$(hostname)
+
+readonly COLLEGE="Tula's"
+
+
+echo "Hello, I am $NAME and my age is $AGE"
+
+
+echo "My college is $COLLEGE"
+
+echo "I am on host $HOSTNAME"
+
+
+
